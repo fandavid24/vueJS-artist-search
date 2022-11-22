b76b52bed3e896cf7063c17ad4ba3c30faac764e
fandavid24 <fandavid@umich.edu>
2022-11-21 22:30:24 -0500

    submission commit

--- script.js
+++ script.js
@@ -3,35 +3,122 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      artist1: './img/1.jpg',
-      artist2: './img/2.jpg',
       term: '',
+      origin: [],
       data: [],
-      genres: {},
+      shown: [],
+      genres: [{
+        'genre': "ALL",
+        'clicked': true
+      }],
+      shownGenres: new Set(),
+      sort: [true, false, false]
     }
   },
   methods: {
     async searchArtist() {
-      const res = await fetch('https://itunes.apple.com/search?entity=song&attribute=artistTerm&term=' + this.term)
+      const res = await fetch('https://itunes.apple.com/search?attribute=allArtistTerm&term=' + this.term)
       const { results } = await res.json()
       this.data = results
+      this.shown = results
+      this.origin = [...results]
       console.log(this.data)
       genreFilter = new Set()
       for (var idx in this.data) {
         // console.log(obj["primaryGenreName"])
         genreFilter.add(this.data[idx]["primaryGenreName"])
       }
-      console.log(genreFilter)
-      this.genres = genreFilter
-
-      console.log(this.genres)
-      // this.data[1]["primaryGenreName"]
-      // this.genreSelect()
+      genreFilter = Array.from(genreFilter);
+      this.genres = [{
+        'genre': "ALL",
+        'clicked': true
+      }]
+      idx = 0
+      for (var entry in genreFilter){
+        temp = {
+          'genre': genreFilter[idx],
+          'clicked': false
+        }
+        this.genres.push(temp)
+        idx++
+      }
+      // console.log(genreFilter)
+      // console.log(this.genres)
     },
-    // genreSelect: function () {
-    //   for (entry in this.data) {
-    //     alert(entry[primaryGenreName])
-    //   }
-    // },
-  }
+    btnClick(index){
+      shownGenres = new Set()
+      this.genres[index]["clicked"] = !this.genres[index]["clicked"]
+      if(index == 0){
+        for (let i = 1; i < this.genres.length; i++) {
+          this.genres[i]['clicked'] = true
+        }
+      }
+      var CLICKED = 0
+      for (let i = 1; i < this.genres.length; i++) {
+        if( this.genres[i]['clicked'] == true){
+          CLICKED = CLICKED + 1
+          shownGenres.add(this.genres[i]['genre'])
+        }
+      }
+      this.shownGenres = shownGenres
+      // console.log(this.shownGenres)
+      if(CLICKED == 0){
+        this.genres[0]['clicked'] = true
+      }
+      else if(CLICKED == this.genres.length-1){
+        this.genres[0]['clicked'] = true
+        for (let i = 1; i < this.genres.length; i++) {
+          this.genres[i]['clicked'] = false
+        }
+      }
+      else{
+        this.genres[0]['clicked'] = false
+      }
+      this.render()
+    },
+    sortBy(index){
+      if(index == 0){
+        this.sort[0] = true
+        this.sort[1] = false
+        this.sort[2] = false
+        this.data = [...this.origin]
+        this.render()
+      }
+      else if(index == 1){
+        this.sort[0] = false
+        this.sort[1] = true
+        this.sort[2] = false
+        this.data.sort((a, b) => { 
+          if(a['collectionName'] < b['collectionName']){return -1} 
+          if(a['collectionName'] > b['collectionName']){return 1} 
+          return 0
+        })
+        this.render()
+      }
+      else if(index == 2){
+        this.sort[0] = false
+        this.sort[1] = false
+        this.sort[2] = true
+        this.data.sort((a, b) => { 
+          if(a['collectionPrice'] < b['collectionPrice']){return -1} 
+          if(a['collectionPrice'] > b['collectionPrice']){return 1} 
+          return 0
+        })
+        this.render()
+      }
+    },
+    render(){
+      if(this.shownGenres.size == 0){
+        this.shown = this.data
+      }
+      else{
+        this.shown = []
+        for (entry of this.data){
+          if(this.shownGenres.has(entry.primaryGenreName)){
+            this.shown.push(entry)
+          }
+        }
+      }
+    }
+  },
 }).mount('#app')
